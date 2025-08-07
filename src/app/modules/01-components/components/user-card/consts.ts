521c841115637485db9569dde2a379bd32660f1b
Den4ik <tarelko_den@list.ru>
2025-08-07 16:06:49 +0300

    feat: added complications to the task with user cards

--- src/app/modules/01-components/components/user-card/consts.ts
+++ src/app/modules/01-components/components/user-card/consts.ts
@@ -3,3 +3,21 @@ export interface User {
 	name: string;
 	email: string;
 }
+
+export const USER_LIST: Array<User> = [
+	{
+		id: 0,
+		name: 'Абоба Абобыч',
+		email: 'aboba@mail.ru'
+	},
+	{
+		id: 1,
+		name: 'Сан Саныч',
+		email: 'sanches@list.ru'
+	},
+	{
+		id: 3,
+		name: 'Анатоль Рыгорыч',
+		email: 'anatol@yandex.ru'
+	}
+];
--- src/app/modules/01-components/components/user-card/user-card.component.ts
+++ src/app/modules/01-components/components/user-card/user-card.component.ts
@@ -1,44 +1,99 @@
-import { ChangeDetectionStrategy, Component /* Input */ } from '@angular/core';
+import { NgIf } from '@angular/common';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	EventEmitter,
+	forwardRef,
+	Input,
+	Output
+} from '@angular/core';
 
-import { User } from './consts';
+import { User, USER_LIST } from './consts';
+
 @Component({
 	selector: 'app-user-card',
 	standalone: true,
 	styleUrls: ['./user-card.component.scss'],
 	templateUrl: './user-card.component.html',
+	imports: [forwardRef(() => UserCardItemComponent), NgIf],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserCardComponent {
-	protected userList: Array<User> = [
-		{
-			id: 0,
-			name: 'Абоба Абобыч',
-			email: 'aboba@mail.ru'
-		},
-		{
-			id: 1,
-			name: 'Сан Саныч',
-			email: 'sanches@list.ru'
-		},
-		{
-			id: 3,
-			name: 'Анатоль Рыгорыч',
-			email: 'anatol@yandex.ru'
+	constructor() {
+		this.resetUserList();
+	}
+
+	protected userList!: Array<User>;
+	public selectedUser: User | undefined;
+
+	public resetUserList(): void {
+		this.userList = Array.from(USER_LIST);
+		this.selectedUser = undefined;
+	}
+
+	public selectUser(inputUser: User) {
+		this.selectedUser = { ...inputUser };
+	}
+
+	public deleteUser(id: number) {
+		this.userList = this.userList.filter(user => user.id !== id);
+
+		if (this.selectedUser?.id === id) {
+			this.selectedUser = undefined;
 		}
-	];
+	}
 }
 
-// eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
 @Component({
 	selector: 'app-user-card-item',
 	standalone: true,
-	template: `<div></div>`,
-	styleUrls: ['./user-card.component.scss']
+	template: `<div class="user-card-item" (click)="select()">
+		<div class="user-card-item__info">
+			<p class="user-card-item__info-line">
+				<span>ID:</span>
+				<span>{{ userInfo.id }}</span>
+			</p>
+			<p class="user-card-item__info-line">
+				<span>Name:</span>
+				<span>{{ userInfo.name }}</span>
+			</p>
+			<p class="user-card-item__info-line">
+				<span>Email:</span>
+				<span>{{ userInfo.email }}</span>
+			</p>
+		</div>
+		<img
+			src="https://i.pravatar.cc/150?u={{ userInfo.name }}"
+			[alt]="'фото ' + userInfo.name"
+		/>
+		<button class="delete-btn" type="button" (click)="selectForDelete($event)">
+			delete
+		</button>
+	</div>`,
+	styleUrls: ['./user-card.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserCardItemComponent {
-	// private _user: User;
+	private _user!: User;
+
+	@Input()
 	set setUser(user: User) {
-		// this._user = { ...user };
+		this._user = { ...user };
 	}
-	// @Input() user: User;
+
+	get userInfo(): User {
+		return this._user;
+	}
+
+	@Output() clickedCard = new EventEmitter<User>();
+	@Output() deletedCard = new EventEmitter<number>();
+
+	select(): void {
+		this.clickedCard.emit(this.userInfo);
+	}
+
+	selectForDelete(event: MouseEvent): void {
+		event.stopPropagation();
+		this.deletedCard.emit(this.userInfo.id);
+	}
 }
